488e5612eea2ac23be84d5d347676189cd22b72f
AnomolyAvery <ashawsolutions@gmail.com>
2025-10-24 19:51:38 -0400

    Added daisyUI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,24 +17,6 @@
 </head>
 
 <body class="h-full">
-    <header class="bg-sky-600 py-4">
-        <nav class="w-full mx-auto container px-2 sm:px-6 md:px-20 flex items-center justify-between">
-            <a href="#" class="text-lg font-bold text-white hover:text-gray-200">{{ config('app.name') }}</a>
-            <ul class="flex-1">
-                <li>
-                    <a href="#">
-                        Store
-                    </a>
-                </li>
-            </ul>
-            <div>
-
-            </div>
-
-            {{-- Mobile Menu --}}
-            <div></div>
-        </nav>
-    </header>
     <main class="py-6">
         <div class="mx-auto container px-2 sm:px-6 md:px-20">
             @yield('content')
